ddf2c65dc3015c38cb781c58d1f509264cadaa5f
Pedrodevv21 <pedroxxdroxx@gmail.com>
2025-07-24 19:27:05 -0300

    commit python

---
+++ workspacee/main.py
@@ -0,0 +1,19 @@
+print ("hellow, world!")
+print (12345, 54321, sep= "-")
+print ((1 == 1))
+print ("eu tenho", 1 + 1)
+
+ano_atual = 2025
+ano_nasc = 2004
+idade = ano_atual - ano_nasc
+print ("quantos anos eu tenho:", idade)
+concatenaçao = print ("pedro" + ' ' "alves" ' '+ str(1))
+pe = "pedro" * 50
+print (pe)
+
+a= "aaaaaaaaaaaaa"
+b= "eeeeeeeeeeeeeeee" 
+formato = "a={} b={}" .format (a, b)
+print (formato)
+
+
---
+++ workspacee/main2.py
@@ -0,0 +1,10 @@
+nome = "pedro"
+sobrenome = "alves"
+data_nasc = 2025 - 2004
+maior_idade = data_nasc >= 18
+altura_em_metros = 1.80
+print ("Nome:", nome)
+print ("Sobrenome:", sobrenome)
+print ("minha idade:", data_nasc)
+print ("sou maior de idade ?", maior_idade)
+print ("minha altura:", altura_em_metros)
