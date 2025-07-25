7ad542702731c68a3791c4711259cf1b5efd26bd
Pedrodevv21 <pedroxxdroxx@gmail.com>
2025-07-24 22:45:43 -0300

    commit day 24

--- workspacee/main.py
+++ workspacee/main.py
@@ -1,12 +1,12 @@
-print ("hellow, world!")
+print ("Hellow, World!")
 print (12345, 54321, sep= "-")
 print ((1 == 1))
-print ("eu tenho", 1 + 1)
+print ("Eu tenho", 1 + 1)
 
 ano_atual = 2025
 ano_nasc = 2004
 idade = ano_atual - ano_nasc
-print ("quantos anos eu tenho:", idade)
+print ("Quantos anos eu tenho:", idade)
 concatenaçao = print ("pedro" + ' ' "alves" ' '+ str(1))
 pe = "pedro" * 50
 print (pe)
---
+++ workspacee/main4.py
@@ -0,0 +1,5 @@
+#input
+
+nome = float (input ("digite um numero:"))
+nome2 = float (input("digite o segundo numero:"))
+print ("A divisão dos dois numeros é:", nome/nome2)
