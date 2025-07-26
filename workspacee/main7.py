87c05d2df6380a693e1d4644c6c6f967e5beda7b
Pedrodevv21 <pedroxxdroxx@gmail.com>
2025-07-26 20:57:13 -0300

    revisei main 7 e main 8
    revisei algumas coisas

--- workspacee/main7.py
+++ workspacee/main7.py
@@ -1,11 +1,11 @@
 #comparaçao de numeros 
 
-primeiro_valor= int(input ("digite um valor:"))
+primeiro_valor= int(input ("digite o primeiro valor:"))
 segundo_valor = int(input("digite o segundo valor:"))
 
-if primeiro_valor < segundo_valor:
-    print("o primeiro valor é menor que valor que o segundo valor")
-elif primeiro_valor > segundo_valor:
+if primeiro_valor > segundo_valor:
+    print("o primeiro valor é maior que o segundo valor")
+elif segundo_valor > primeiro_valor:
     print ("o segundo valor e maior que o primeiro valor")
 elif primeiro_valor == segundo_valor:
     print ("os valores são iguais")
--- workspacee/main8.py
+++ workspacee/main8.py
@@ -4,11 +4,15 @@ Primeiro_valor = int (input ("digite o primeiro valor:"))
 Segundo_valor = int (input ("digite o segundo valor:"))
 Terceiro_valor = int (input ("digite o terceiro valor:"))
 
-if Primeiro_valor > Segundo_valor and Terceiro_valor:
+if Primeiro_valor > Segundo_valor and Primeiro_valor > Terceiro_valor:
     print ("maior valor e o primeiro")
-elif Segundo_valor > Terceiro_valor and Primeiro_valor:
+elif Segundo_valor > Terceiro_valor and Segundo_valor > Primeiro_valor:
     print ("maior valor e o segundo") 
-elif Terceiro_valor > Primeiro_valor and Segundo_valor:
+elif Terceiro_valor > Primeiro_valor and Terceiro_valor > Segundo_valor:
     print ("maior valor e o terceiro") 
+elif Primeiro_valor == Segundo_valor and Primeiro_valor > Terceiro_valor:
+    print ("os primeiro e o segundo valor são iguais")
+elif Segundo_valor == Terceiro_valor and Segundo_valor > Primeiro_valor:
+    print ("o segundo e o terceiro valor são iguais")
 else:
     print ("os valores são iguais")
