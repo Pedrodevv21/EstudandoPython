2bf8e2a56f7910ca697e958613901f2e6618fc3e
Pedrodevv21 <pedroxxdroxx@gmail.com>
2025-08-02 22:09:58 -0300

    +pratica+constancia

---
+++ Anotaçoes/main_ingles.py
@@ -0,0 +1,3 @@
+print ("Hello, my name is Pedro.")
+print ("I'm learning english to work in tech")
+print ("Step by step, every day")
---
+++ Exercicios/horas.py
@@ -0,0 +1,11 @@
+horas_inteiros = int (input ("Que horas são ? "))
+
+if horas_inteiros >= 0 and horas_inteiros <= 11:
+    print ("Bom dia!")
+elif horas_inteiros >= 12 and horas_inteiros <= 17:
+    print ("Boa tarde!")
+elif horas_inteiros >= 18 and horas_inteiros <= 23:
+    print ("Boa noite!")
+else:
+    print ("Não conheço essa hora")
+    
