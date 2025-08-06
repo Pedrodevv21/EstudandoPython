a63730da71050024feb4f4e519386315e0c8f48d
Pedro <Pedroxxdroxx@gmail.com>
2025-08-05 22:02:27 -0300

    contância

---
+++ Exercicios/numero_secreto.py
@@ -0,0 +1,16 @@
+
+numero_secreto = 7
+numero_usuario = int(input ("digite um numero: "))
+
+if numero_usuario == 7:
+    print ("Você acertou o numero secreto")
+elif numero_usuario > 7:
+    print ("Voçê errou! o seu numero precisa ser menor")
+elif numero_usuario < 7:
+    print ("Voçe errou! seu numero precisa ser maior")
+else:
+    ("Voçe não digitou nada ou voçe nap digitou um  numero inteiro")
+
+
+
+
