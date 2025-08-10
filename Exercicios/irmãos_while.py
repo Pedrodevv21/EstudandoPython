2ef902958f81907fce225b36605f04f54c8cefa3
Pedro <Pedroxxdroxx@gmail.com>
2025-08-09 22:10:39 -0300

    aprendendo while

---
+++ Exercicios/irmãos_while.py
@@ -0,0 +1,8 @@
+nome_irmãos = (input ("digite o nome dos seus irmãos: "))
+
+while nome_irmãos:
+    print (f"o nome dos seus irmãos é {nome_irmãos}")
+
+    if nome_irmãos == "sair":
+        break  
+print ("acabou")
